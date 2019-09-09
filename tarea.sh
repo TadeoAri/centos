27b8ad86ebc2e5565ca5b73584a00c282f9cdd4a
TadeoAri <54553587+TadeoAri@users.noreply.github.com>
2019-09-08 23:17:45 -0500

    Add files via upload

---
+++ tarea.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Bienvenidos a CentOS!"
+
+mkdir -p Tarea1
+
+echo "Hola esta es nuestra información dentro de nuestro fichero" < "Tarea1/Capcom.txt"
+
+cd Tarea1
+
+cp Capcom.txt Capcom2.txt
+
+cp Capcom.txt lab.txt
+
+mv lab.txt Capcom3.txt
+
+ls -all -h
